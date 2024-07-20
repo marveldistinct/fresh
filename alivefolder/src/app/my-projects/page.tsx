5435be5ddb42f9c153b23614ad09d7e6696c843e
MarvelDistinct <macbook@Marvel-Mac.local>
2024-07-20 12:36:09 -0700

    my skills page

--- alivefolder/src/app/my-projects/page.tsx
+++ alivefolder/src/app/my-projects/page.tsx
@@ -1,4 +1,10 @@
+"use client"
 import React from 'react'
+import {Swiper, SwiperSlide} from "swiper/react"
+import "swiper/css"
+import { SkillData } from '../../../constants'
+import Image from 'next/image'
+import {Autoplay} from "swiper/modules" 
 
 const My projects = () => {
   return (
