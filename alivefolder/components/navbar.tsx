37e8a8beb28066933e6fc3e491269a92bc159096
MarvelDistinct <macbook@Marvel-Mac.local>
2024-07-20 10:13:43 -0700

    Navbar First Draft

--- alivefolder/components/navbar.tsx
+++ alivefolder/components/navbar.tsx
@@ -8,6 +8,7 @@ const Navbar =() => {
     return (
         <div className="fixed top-0 z-[40] w-full h-[100px] bg-transparent flex justify-between items-center px-10 md:px-20">
                 <div className="flex flex-row gap-3 items-center">
+                    <div className="relative">
                     <Image
                         src="/horseLogo.jpg"
                         alt="logo"
@@ -15,6 +16,7 @@ const Navbar =() => {
                         height={40}
                         className="w-full h-full object-contain rounded-full"
                     />
+                    </div>
                 <h1 className="text-white text-[25px] font-semibold">Webchain Dev</h1>
                 </div>
 
